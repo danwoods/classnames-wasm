44b15b406bc13a348c98e065124030c5199e6f16
Dan Woodson <woodson.dan@gmail.com>
2024-05-30 11:58:29 -0600

    Working in browser

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
-// import "fmt"
+//import "fmt"
 
 
 func main() { 
-// obj := map[string]bool{"name": true} 
-  // classnames("hello", "Change", "the ", "World using Golang", obj)
+  //println(str)
+obj := map[string]bool{"name": true} 
+classnames("hello", "Change", "the ", "World using Golang", obj)
 }
 
-func classnames(messages ...interface{}) {
-  for _, i := range messages {
-    println(i)
-  }
-}
+ func classnames(messages ...interface{}) {
+   for _, i := range messages {
+     println(i)
+   }
+ }
 
