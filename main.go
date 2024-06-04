a06fc4ed316583e07be05fc5fbdfb1a078e59fff
Dan Woodson <woodson.dan@gmail.com>
2024-06-04 15:22:35 -0600

    Printing from inside go!

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ package main
 
 func main() {
 	//println(str)
-	obj := map[string]bool{"name": true}
-	classnames("hello", "Change", "the ", "World using Golang", obj)
+	// obj := map[string]bool{"name": true}
+	// classnames("hello", "Change", "the ", "World using Golang", obj)
+	// <-make(chan bool)
 }
 
 //export classnames
 func classnames(messages ...interface{}) {
+	println("here")
+	println(messages)
 	for _, i := range messages {
 		println(i)
 	}
